69c98900be0718a5174d6d2d602dca6ada687bee
Baptiste7905 <baptisteaudouin.ba@gmail.com>
2022-11-29 10:32:29 +0100

    Table des cartes

--- db.js
+++ db.js
@@ -4,3 +4,19 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: path.join(__dirname, 'db.sqlite')
 });
+
+const cartes = sequelize.define('cartes', {
+    nom : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    position_c: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    position_l: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+});
+
